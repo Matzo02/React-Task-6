f7adab223be7054e1f67df264bab80df63ef700b
unknown <Ishwaq>
2023-10-16 23:03:49 +0530

    first commit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,13 +9,8 @@ export default function Projects() {
         <div className="flex flex-col w-full mb-20">
           <CodeIcon className="mx-auto inline-block w-10 mb-4" />
           <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-white">
-            Apps I've Built
+            Projects
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo
-            facilis repellat ab cupiditate alias vero aliquid obcaecati quisquam
-            fuga dolore.
-          </p>
         </div>
         <div className="flex flex-wrap -m-4">
           {projects.map((project) => (
@@ -26,10 +21,10 @@ export default function Projects() {
               <div className="flex relative">
                 <img
                   alt="gallery"
-                  className="absolute inset-0 w-full h-full object-cover object-center"
+                  className="absolute inset-0 w-full h-full object-none"
                   src={project.image}
                 />
-                <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <div className="px-8 py-40 z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-90">
                   <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
                     {project.subtitle}
                   </h2>
--- src/data.js
+++ src/data.js
@@ -1,34 +1,23 @@
 export const projects = [
   {
-    title: "React Reserve",
+    title: "Calculator",
     subtitle: "MERN Stack",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-1.gif",
+    image: "https://c4.wallpaperflare.com/wallpaper/260/699/743/dark-drizzle-lights-mist-wallpaper-preview.jpg",
     link: "https://reactbootcamp.com",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
+    title: "Color Picker",
+    subtitle: "MERN Stack",
+    image: "https://c1.wallpaperflare.com/preview/748/977/304/storm-sky-cloudy-weather.jpg",
     link: "https://reedbarger.com",
   },
-  {
-    title: "DevChat",
-    subtitle: "React and Firebase",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://jsbootcamp.com",
-  },
-  {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://pythonbootcamp.com",
-  },
+
+  
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Python",
+  "Html",
 ];
